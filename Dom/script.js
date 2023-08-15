d59ac2ffc09beaaed6f00739fc911dc89d08988f
Aman Kumar Singh <112749591+amansingh-gh@users.noreply.github.com>
2023-08-16 02:19:31 +0530

    Update script.js

--- Dom/script.js
+++ Dom/script.js
@@ -1,43 +1,43 @@
-// // getElementId selector
-// const mainHeading = (document.getElementById("main-heading"))
-// // console.log(mainHeading)
-// // mainHeading.style.color="red"
+// getElementId selector
+const mainHeading = (document.getElementById("main-heading"))
+// console.log(mainHeading)
+// mainHeading.style.color="red"
 
-// // query selector-> only gives only first matched class
-// const header = document.querySelector(".header")
-// console.log(header)
+// query selector-> only gives only first matched class
+const header = document.querySelector(".header")
+console.log(header)
 
-// // query selectorAll -> gives nodeList of all same class item
-// const navItem = document.querySelectorAll(".nav-item")
-// console.log(navItem)
-
-
-// // Now changing something
-// const newMainHeading = document.getElementById("main-heading");
-// console.log(newMainHeading.textContent);
-// // newMainHeading.textContent = "This is new Text Changed by console"
-// console.log(newMainHeading.textContent);
+// query selectorAll -> gives nodeList of all same class item
+const navItem = document.querySelectorAll(".nav-item")
+console.log(navItem)
 
 
-// // changing the font color
-// const newColor = document.querySelector(".section-todo h2");
-// newColor.style.color = "red";
-// // changing the background color
-// newColor.style.backgroundColor = "royalblue"
+// Now changing something
+const newMainHeading = document.getElementById("main-heading");
+console.log(newMainHeading.textContent);
+// newMainHeading.textContent = "This is new Text Changed by console"
+console.log(newMainHeading.textContent);
+
+
+// changing the font color
+const newColor = document.querySelector(".section-todo h2");
+newColor.style.color = "red";
+// changing the background color
+newColor.style.backgroundColor = "royalblue"
 
 
 
-// // get and set attribute
-// const link = document.querySelector("a");
-// console.log(link.getAttribute("href").slice(1))
+// get and set attribute
+const link = document.querySelector("a");
+console.log(link.getAttribute("href").slice(1))
 
 
-// const link2 = document.querySelector(".form-todo input")
-// console.log(link2.getAttribute("type"))
+const link2 = document.querySelector(".form-todo input")
+console.log(link2.getAttribute("type"))
 
 
-// console.log(link2.setAttribute("placeholder" , "Input something new"))
-// console.log(link2.getAttribute("placeholder"))
+console.log(link2.setAttribute("placeholder" , "Input something new"))
+console.log(link2.getAttribute("placeholder"))
 
 
 
@@ -45,17 +45,15 @@
 
 // get multiple elements using getElemets by using class name -> getElementByClassName
 
-// const navItems = document.getElementsByClassName("nav-item"); 
-// // upper line returns HTML collection -we cannot use forEach loop in HTML collection
-// console.log(navItems[0], navItems[1])
+const navvItems = document.getElementsByClassName("nav-item"); 
+// upper line returns HTML collection -we cannot use forEach loop in HTML collection
+console.log(navvItems[0], navItems[1])
 
 
-// // get multiple elements items using querySelectorAll
-// const qAll = document.querySelectorAll(".nav-item")
-// console.log(qAll[1])
+// get multiple elements items using querySelectorAll
+const qAll = document.querySelectorAll(".nav-item")
+console.log(qAll[1])
 // upper line returns node list
-
-
 
 
 
@@ -68,11 +66,11 @@
 const navItems = document.getElementsByTagName("a");
 
 // for loop
-// for(let i=0; i<navItems.length; i++){
-//     const navItemss = navItems[i];
-//     navItemss.style.backgroundColor = "blue"
-//     navItemss.style.color ="red"
-// }
+for(let i=0; i<navItems.length; i++){
+    const navItemss = navItems[i];
+    navItemss.style.backgroundColor = "blue"
+    navItemss.style.color ="red"
+}
 
 // for of loop
 for(let navItm of navItems){
