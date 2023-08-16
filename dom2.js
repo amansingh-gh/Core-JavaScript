5ad5f087fda9c8b7d7f132766f504bd24a49af52
Aman Kumar Singh <112749591+amansingh-gh@users.noreply.github.com>
2023-08-17 01:56:22 +0530

    Delete dom2.js

--- dom2.js
+++
@@ -1,6 +0,0 @@
-// inner HTML
-const headline = document.querySelector(".headline");
-console.log(headline);
-headline.innerHTML="<h1>Hey this is me</h1>";
-headline.innerHTML += "<button class=\'btn\'>This is button comes from</button>";
-console.log(headline);
