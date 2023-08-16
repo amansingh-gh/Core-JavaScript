964d544e475ddffc6c563ab3173ff91b6fe1b151
Aman Kumar Singh <112749591+amansingh-gh@users.noreply.github.com>
2023-08-17 01:53:20 +0530

    Add files via upload

---
+++ Dom/evt.js
@@ -0,0 +1,39 @@
+const grandparent = document.querySelector(".grandparent");
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+
+// // Captured
+child.addEventListener("click", () => {
+    console.log("capture !!!! child");
+}, true);
+parent.addEventListener("click", () => {
+    console.log("capture !!!! parent");
+}, true);
+grandparent.addEventListener("click", () => {
+    console.log("capture !!!! grandparent");
+},true);
+document.body.addEventListener("click", () => {
+    console.log("capture !!!! document.body");
+}, true);
+
+
+// Not captured
+child.addEventListener("click", () => {
+  console.log("bubble child");
+});
+parent.addEventListener("click", () => {
+  console.log("bubble parent");
+});
+grandparent.addEventListener("click", () => {
+  console.log("bubble grandparent");
+});
+document.body.addEventListener("click", () => {
+  console.log("bubble document.body");
+});
+
+
+
+// event delegation
+grandparent.addEventListener("click", (e) => {
+  console.log(e.target);
+});
